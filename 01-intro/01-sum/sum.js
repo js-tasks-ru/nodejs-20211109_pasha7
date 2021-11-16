edb7dabab4ebcbbb174bc4aaebb989b63e3987a0
Pasha Frolov <pfrlvgg@gmail.com>
2021-11-16 15:33:33 +0300

    feat: add solution for 01-01

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,9 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new TypeError('Only numbers are allowed')
+  }
+
+  return a + b
 }
 
-module.exports = sum;
+module.exports = sum
